615c7575c385267288cc888c48a84ee3862667f1
mattetraed <peter7328matyas+vince@gmail.com>
2026-05-08 21:03:23 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then(list => {
+      for (const client of list) {
+        if (client.focus) return client.focus();
+      }
+    })
+  );
+});
